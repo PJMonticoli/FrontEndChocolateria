566b68fe5d23678b82a11575e91e4e76d9f2b64d
PJMonticoli <pablo_monticoli@hotmail.com>
2025-03-20 21:34:50 -0300

    wip home styles. Improve image transition and update background images

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    let slides = document.querySelectorAll('.slide');
+    let index = 0;
 
+    setInterval(() => {
+      slides[index].classList.remove('active'); 
+      index = (index + 1) % slides.length; 
+      slides[index].classList.add('active'); 
+    }, 7000); 
+  }
 }
