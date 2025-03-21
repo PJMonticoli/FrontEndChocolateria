01b1c3a860f6e13668cd35ee21d4e6821720fe80
PJMonticoli <pablo_monticoli@hotmail.com>
2025-03-20 22:56:33 -0300

    WIP update more table styles and filter pipes

--- src/app/pipes/filtro-canjeadas.pipe.ts
+++ src/app/pipes/filtro-canjeadas.pipe.ts
@@ -7,12 +7,12 @@ import { DtoPromociones } from '../models/dto-promociones';
 export class FiltroCanjeadasPipe implements PipeTransform {
   transform(promocion: DtoPromociones[], page: number=0, search : string): DtoPromociones[] {
     if(search.length === 0){
-      return promocion.slice(page, page+5);
+      return promocion.slice(page, page+8);
     }
     const promocionsFiltradas = promocion.filter(promo=>  promo.nombreSocio.normalize('NFD').toLowerCase()
     .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search) 
     || promo.nombrePromocion.normalize('NFD').toLowerCase()
     .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1").includes(search));
-    return promocionsFiltradas.slice(page, page+5);
+    return promocionsFiltradas.slice(page, page+8);
   }
 }
